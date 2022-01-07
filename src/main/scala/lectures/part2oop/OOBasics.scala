0607a3be22a6a16c4469b5f27ff15205fb7e759e
sanketnimbalkar <sanath4096@gmail.com>
2022-01-07 14:28:28 +0530

    Import statement removed from OOBasics.scala

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,7 +1,5 @@
 package scalaoops
 
-import scalaoops.practiceexercise.{Counter, Novel, Person, Writer}
-
 object OOBasics extends App {
   val person = new Person("Sanket", 25) //Class instantiation
   println(person.x)
@@ -10,10 +8,10 @@ object OOBasics extends App {
 }
 
 /**
-  * Constructor
-  * Class parameters are NOT FIELDS ex age in above case
-  * To convert into fields add val or var
-  */
+ * Constructor
+ * Class parameters are NOT FIELDS ex age in above case
+ * To convert into fields add val or var
+ */
 
 class Person(name: String, val age: Int) {
   //body
@@ -22,31 +20,31 @@ class Person(name: String, val age: Int) {
   println(1 + 3) // At every instantiation the whole block is evaluated.
 
   /**
-    * method
-    * $name refers to method parameter
-    */
+   * method
+   * $name refers to method parameter
+   */
 
   def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
 
   /**
-    * $name refers to class parameter. This is also method overloading.
-    */
+   * $name refers to class parameter. This is also method overloading.
+   */
   def greet(): Unit = println(s"Hi, I am $name")
 
   /**
-    * multiple constructors or overloading constructors.
-    */
+   * multiple constructors or overloading constructors.
+   */
 
   def this(name: String) = this(name, 0)
 
   /**
-    * auxillary constructor calls primary constructors - Used only for default parameters.
-    * We do not need this as we can supply default parameter to primary constructor.
-    */
+   * auxillary constructor calls primary constructors - Used only for default parameters.
+   * We do not need this as we can supply default parameter to primary constructor.
+   */
   def this() = this("Sanket")
 
   /**
-    * only imlementation the auxillary constructor can have
-    */
+   * only imlementation the auxillary constructor can have
+   */
 
 }
